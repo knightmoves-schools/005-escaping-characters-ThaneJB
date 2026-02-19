6a4b08063d952f9112769bdf7bc946049324f2a7
Thane <thabay0606@students.maquoketaschools.org>
2026-02-19 14:25:33 -0600

    Forgot @

--- Email.cs
+++ Email.cs
@@ -2,7 +2,7 @@
 public class Email
 {
     // Add your code here
-    public const string Introduction = "Hello Manny, \n" +
+    public const string Introduction = @"Hello Manny, \n" +
     "\tWhy did you \"take care of\" Stan?\n" + 
     "He was supposed to be the \'decoy\'.\n\n" +
     "Goodbye, Manny :\\";
