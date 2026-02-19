41e2110edfde5bb68bdfcd36e2cabfc077bb5472
Thane <thabay0606@students.maquoketaschools.org>
2026-02-19 14:23:10 -0600

    Added new line, tab, single quote, double quote, and backslash to introduction.

--- Email.cs
+++ Email.cs
@@ -2,4 +2,8 @@
 public class Email
 {
     // Add your code here
+    public const string introduction = "Hello Manny, \n" +
+    "\tWhy did you \"take care of\" Stan?\n" + 
+    "He was supposed to be the \'decoy\'.\n\n" +
+    "Goodbye, Manny :\\";
 }
