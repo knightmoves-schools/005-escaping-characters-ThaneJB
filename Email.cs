f94cffd68768feed0c7d1252eb8486e7bd956846
Thane <thabay0606@students.maquoketaschools.org>
2026-02-23 13:54:11 -0600

    Removed const from string.

--- Email.cs
+++ Email.cs
@@ -2,7 +2,7 @@
 public class Email
 {
     // Add your code here
-    public const string Introduction = "Hello Manny, \n" +
+    public string Introduction = "Hello Manny, \n" +
     "\tWhy did you \"take care of\" Stan.\n" + 
     "He was supposed to be the \'decoy\'.\n" +
     "Goodbye, Manny :\\";
